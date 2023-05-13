fcc3b12871316a515ef5a583e8a0fd8f7be34585
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2023-05-13 10:21:13 +0530

    Updated .github

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,8 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-  bracketSameLine: true,
-  trailingComma: 'all',
-  semi: false,
-  singleQuote: true,
-}
+	bracketSameLine: true,
+	trailingComma: "all",
+	semi: false,
+	singleQuote: true,
+	printWidth: 120,
+};
